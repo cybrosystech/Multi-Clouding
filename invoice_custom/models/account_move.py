6a27b80663a2bc259f32012ab1cd466b8dd80cbe
Mohamed Sherif <mosherif91@gmail.com>
2021-10-24 14:09:18 +0200

    [ADD] invoice custom

---
+++ invoice_custom/models/account_move.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import math
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from odoo.tools import float_compare, float_round
+from odoo.tools.misc import formatLang
+from dateutil.relativedelta import relativedelta
+from datetime import date
+import random
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    def create(self):
+        todays_date = date.today()
+        res = super(AccountMove, self).create()
+        # Invoice
+        str = ''
+        if self.move_type == 'out_invoice':
+            str = 'INV/'+todays_date.year+'/'+todays_date.month+'/'+todays_date.day+''+random.randint(1,10)
+        # Bill
+        if self.move_type == 'in_invoice':
+            str = 'BILL/'+todays_date.year+'/'+todays_date.month+'/'+todays_date.day+''+random.randint(1,10)
+
+        self.name = str
+        return res
