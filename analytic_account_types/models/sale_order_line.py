6a71bc01ca8e27e22c1e9a4f6dc254eb7de1ac16
Mohamed Sherif <mosherif91@gmail.com>
2021-11-17 13:56:20 +0200

    [UPD] sale order line

--- analytic_account_types/models/sale_order_line.py
+++ analytic_account_types/models/sale_order_line.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError,UserError
 
 
 class SaleOrder(models.Model):
