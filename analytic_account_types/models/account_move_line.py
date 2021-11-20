2b7acc660917d4673cc0f61b7aa83440ac3db448
pavel-orlov <porlov@tasctowers.com>
2021-11-20 20:55:08 +0400

    set invoice display name when in_approve status

--- analytic_account_types/models/account_move_line.py
+++ analytic_account_types/models/account_move_line.py
@@ -56,7 +56,29 @@ class AccountMove(models.Model):
                 rec.new_sequence = self.env['ir.sequence'].next_by_code('entry.temporary.seq')
         return res
 
-
+    # Override native method to set invoice display name in 'to_approve'status.
+    def _get_move_display_name(self, show_ref=False):
+        ''' Helper to get the display name of an invoice depending of its type.
+        :param show_ref:    A flag indicating of the display name must include or not the journal entry reference.
+        :return:            A string representing the invoice.
+        '''
+        self.ensure_one()
+        draft_name = ''
+        if self.state in ('draft', 'to_approve'):
+            draft_name += {
+                'out_invoice': _('Draft Invoice'),
+                'out_refund': _('Draft Credit Note'),
+                'in_invoice': _('Draft Bill'),
+                'in_refund': _('Draft Vendor Credit Note'),
+                'out_receipt': _('Draft Sales Receipt'),
+                'in_receipt': _('Draft Purchase Receipt'),
+                'entry': _('Draft Entry'),
+            }[self.move_type]
+            if not self.name or self.name == '/':
+                draft_name += ' (* %s)' % str(self.id)
+            else:
+                draft_name += ' ' + self.name
+        return (draft_name or self.name) + (show_ref and self.ref and ' (%s%s)' % (self.ref[:50], '...' if len(self.ref) > 50 else '') or '')
 
 
 
