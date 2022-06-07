8fd41e8aa12de7baf30e349ead731d975694cbda
cybrosys <cybrosys@cybrosy>
2022-06-07 16:12:00 +0530

    [ADD] analatycal_type, approve_status

---
+++ approve_status/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Approve Status',
+    'summary': """""",
+
+    'description': """""",
+
+    'author': "",
+    'website': "",
+    'category': 'Accounting',
+    'version': '14.0.1.0.0',
+    'depends': ['account', 'analytic_account_types'],
+
+    'data':
+        [
+            'view/reset_to_draft.xml',
+            'view/approve_status.xml'
+        ]
+}
