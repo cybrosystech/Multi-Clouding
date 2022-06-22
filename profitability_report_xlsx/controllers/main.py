8b6b8484382bfa9e2a16f9f1ca38f8339f0700d6
cybrosys <cybrosys@cybrosy>
2022-06-22 13:26:55 +0530

    [UPDATE] profitability_report action_manager

--- profitability_report_xlsx/controllers/main.py
+++ profitability_report_xlsx/controllers/main.py
@@ -9,10 +9,11 @@ class XLSXReportController(http.Controller):
 
     @http.route('/xlsx_reports', type='http', auth='user', methods=['POST'],
                 csrf=False)
-    def get_xlsx_report(self, model, options, output_format, token, report_name,
+    def get_report_xlsx(self, model, options, output_format, token, report_name,
                         **kw):
         uid = request.session.uid
-        report_obj = request.env[model].with_user(uid)
+        print(model, 'model')
+        report_obj = request.env[model].sudo(uid)
         options = json.loads(options)
         try:
             if output_format == 'xlsx':
--- profitability_report_xlsx/static/src/js/action_manager.js
+++ profitability_report_xlsx/static/src/js/action_manager.js
@@ -5,19 +5,19 @@ odoo.define('profitability_report_xlsx.ActionManager', function (require) {
  * The purpose of this file is to add the actions of type
  * 'ir_actions_xlsx_download' to the ActionManager.
  */
-
 var ActionManager = require('web.ActionManager');
 var framework = require('web.framework');
 var session = require('web.session');
+
 ActionManager.include({
 
-    /**
-     * Executes actions of type 'ir_actions_xlsx_download'.
+     /**
+     * Executes actions of type 'ir.actions.report'.
      *
      * @private
      * @param {Object} action the description of the action to execute
-     * @returns {Deferred} resolved when the report has been downloaded ;
-     *   rejected if an error occurred during the report generation
+     * @param {Object} options @see doAction for details
+     * @returns {Promise} resolved when the action has been executed
      */
     _executexlsxReportDownloadAction: function (action) {
         framework.blockUI();
@@ -31,15 +31,14 @@ ActionManager.include({
         });
         return def;
     },
-    /**
-     * Overrides to handle the 'ir_actions_xlsx_download' actions.
+   /**
+     * Overrides to handle the 'ir.actions.report' actions.
      *
      * @override
      * @private
      */
-    _handleAction: function (action, options) {
-
-        if (action.type === 'ir_actions_xlsx_download') {
+    _executeReportAction: function (action, options) {
+        if (action.report_type === 'xlsx') {
             return this._executexlsxReportDownloadAction(action, options);
         }
         return this._super.apply(this, arguments);
