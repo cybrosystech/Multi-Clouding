0fcf6705c77c943b39bf115f64a5e586642fc4a4
cybrosys <cybrosys@cybrosy>
2022-07-13 14:48:36 +0530

    [UPDATE] invoice_tax_report

---
+++ invoice_tax_report/reports/inv_tax_report.py
@@ -0,0 +1,37 @@
+from odoo import models, api, fields
+
+
+class ReportInvoiceTaxReport(models.AbstractModel):
+    _name = 'report.invoice_tax_report.inv_tax_report'
+    _description = 'Invoice Tax report'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['account.move'].browse(docids)
+        print(docs.currency_id.name)
+        to_currency = self.env['res.currency'].search(
+            [('name', '=', 'AED')])
+        rate = docs.company_currency_id._get_conversion_rate(
+            docs.currency_id, to_currency, docs.company_id,
+            docs.date)
+        print('rate', rate)
+        return {
+            'doc_ids': docids,
+            'doc_model': 'account.move',
+            'docs': docs,
+            'report_type': data.get('report_type') if data else '',
+            'exchange_rate': rate
+        }
+
+# class ReportInvoiceWithPayment(models.AbstractModel):
+#     _name = 'report.account.report_invoice_with_payments'
+#     _description = 'Account report with payment lines'
+#
+#     @api.model
+#     def _get_report_values(self, docids, data=None):
+#         return {
+#             'doc_ids': docids,
+#             'doc_model': 'account.move',
+#             'docs': self.env['account.move'].browse(docids),
+#             'report_type': data.get('report_type') if data else '',
+#         }
