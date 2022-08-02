e31896813f6f93281b0cd2baef1aaaf0b821d86b
cybrosys <cybrosys@cybrosy>
2022-08-02 17:05:19 +0530

    [UPDATE] colocation onchange

--- analytic_account_colocation_type/models/analytic_account_inherit.py
+++ analytic_account_colocation_type/models/analytic_account_inherit.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -9,6 +9,10 @@ class AnalyticAccount(models.Model):
 
     analytic_account_type = fields.Selection(selection_add=[('co_location',
                                                              'Co Location')])
+    co_location = fields.Many2one('account.analytic.account',
+                                  string="Co location",
+                                  domain=[('analytic_account_type', '=',
+                                           'co_location')])
 
 
 class AccountAssetInherit(models.Model):
@@ -20,6 +24,12 @@ class AccountAssetInherit(models.Model):
                                            'co_location')],
                                   required=False)
 
+    @api.onchange('project_site_id')
+    def _onchange_project_site_id(self):
+        for rec in self:
+            rec.co_location = rec.project_site_id.co_location.id
+        return super(AccountAssetInherit, self)._onchange_project_site_id()
+
 
 class AccountMoveLineInherit(models.Model):
     _inherit = 'account.move.line'
@@ -27,6 +37,12 @@ class AccountMoveLineInherit(models.Model):
     co_location_id = fields.Many2one(comodel_name="account.analytic.account",
                                      string="Co location", domain=[(
             'analytic_account_type', '=', 'co_location')], required=False, )
+
+    @api.onchange('project_site_id')
+    def get_location_and_types(self):
+        for rec in self:
+            rec.co_location_id = rec.project_site_id.co_location.id
+        return super(AccountMoveLineInherit, self).get_location_and_types()
 
 
 class AccountAnalyticLineInherit(models.Model):
@@ -50,6 +66,12 @@ class PurchaseOrderLineCoLocation(models.Model):
                                      string="Co Location", domain=[
             ('analytic_account_type', '=', 'co_location')], required=False, )
 
+    @api.onchange('project_site_id')
+    def get_location_and_types(self):
+        for rec in self:
+            rec.co_location_id = rec.project_site_id.co_location.id
+        return super(PurchaseOrderLineCoLocation, self).get_location_and_types()
+
 
 class SalesOrderLineInheritCoLocation(models.Model):
     _inherit = 'sale.order.line'
@@ -57,3 +79,10 @@ class SalesOrderLineInheritCoLocation(models.Model):
     co_location_id = fields.Many2one(comodel_name="account.analytic.account",
                                      string="Co Location", domain=[
             ('analytic_account_type', '=', 'co_location')], required=False, )
+
+    @api.onchange('project_site_id')
+    def get_location_and_types(self):
+        for rec in self:
+            rec.co_location_id = rec.project_site_id.co_location.id
+        return super(SalesOrderLineInheritCoLocation,
+                     self).get_location_and_types()
