e49524ec4e23813bcdf009a9a9defb1bfd031672
cybrosys <cybrosys@cybrosy>
2022-09-11 19:31:56 +0530

    [ADD] bank_reconcilation_report

---
+++ bank_reconcilation_report/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Bank Reconciliation Report Inherit',
+    'summary': """""",
+
+    'description': """""",
+
+    'author': "",
+    'website': "",
+    'category': 'Accounting',
+    'version': '14.0.1.0.0',
+    'depends': ['account_reports'],
+
+    'data':
+        []
+}
