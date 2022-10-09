8dcf53cd0973fdececcafa645e3122262f64cfd5
cybrosys <cybrosys@cybrosy>
2022-10-09 16:11:53 +0530

    [NEW] account_deferred_report

---
+++ account_deferred_report/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Account Deferred Report',
+    'summary': """""",
+
+    'description': """Assets Report Inherit, This module is 
+    for the management of deferred Expenses""",
+
+    'author': "",
+    'website': "",
+    'category': 'Accounting',
+    'version': '14.0.1.0.0',
+    'depends': ['account_asset', 'account_reports'],
+
+    'data':
+        [
+            'report/account_deferred_expense_report_view.xml',
+        ]
+}
