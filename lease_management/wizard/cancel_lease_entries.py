69e3944dae064305e8066a1b801273fbcbb377ec
cybrosys <cybrosys@cybrosy>
2022-10-09 13:32:06 +0530

    [FIX] Lease_management calculation issue

--- lease_management/wizard/cancel_lease_entries.py
+++ lease_management/wizard/cancel_lease_entries.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """ init object """
-from odoo import fields, models, api, _ ,tools, SUPERUSER_ID
-from odoo.exceptions import ValidationError,UserError
-from datetime import datetime , date ,timedelta
+from odoo import fields, models, api, _, tools, SUPERUSER_ID
+from odoo.exceptions import ValidationError, UserError
+from datetime import datetime, date, timedelta
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
 from dateutil.relativedelta import relativedelta
@@ -17,9 +17,12 @@ class CancelLeaseEntries(models.TransientModel):
     _name = 'cancel.lease.entries'
     _description = 'Cancel Lease Entries'
 
-    date = fields.Date(default=lambda self: fields.Datetime.now(), required=True, )
+    date = fields.Date(default=lambda self: fields.Datetime.now(),
+                       required=True, )
 
     def action_apply(self):
+        print(self.env.company.id)
+        print(self.env.company.name)
         account_moves = self.env['account.move'].search([
             ('date', '<', self.date),
             ('state', '!=', 'cancel'),
@@ -28,14 +31,15 @@ class CancelLeaseEntries(models.TransientModel):
             '&',
             ('asset_id', '!=', False),
             ('asset_id.leasee_contract_ids', '!=', False),
+            ('company_id', '=', self.env.company.id)
         ]).filtered(lambda m: m.date < self.date)
         # account_moves.button_draft()
+        print('account_moves', account_moves)
         account_moves.button_cancel()
         payments = self.env['account.payment'].search([
             ('date', '<', self.date),
             ('state', '!=', 'cancel'),
             ('is_leasee_payment', '!=', False),
+            ('company_id', '=', self.env.company.id)
         ]).filtered(lambda m: m.date < self.date)
         payments.action_cancel()
-
-
