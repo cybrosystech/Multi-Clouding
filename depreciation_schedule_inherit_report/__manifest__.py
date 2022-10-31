429889faabe854b45b4c2c8e43e80f711c6bbd2e
cybrosys <cybrosys@cybrosy>
2022-10-31 19:04:12 +0530

    [ADD] depreciation_schedule_inherit_report

---
+++ depreciation_schedule_inherit_report/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Assets Report Inherit',
+    'summary': """""",
+
+    'description': """""",
+
+    'author': "",
+    'website': "",
+    'category': 'Accounting',
+    'version': '14.0.1.0.0',
+    'depends': ['account_asset'],
+
+    'data':
+        [
+            # 'views/generic_tax_report_inherit.xml',
+        ]
+}
