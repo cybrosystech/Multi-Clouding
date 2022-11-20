9c2850a7d0860de8ba1dc192f2a135839f815eef
cybrosys <cybrosys@cybrosy>
2022-11-20 15:43:19 +0530

    [ADD] asset_partial_sale

---
+++ assets_partial_sale/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Partial Assets Sale',
+    'summary': """""",
+
+    'description': """""",
+
+    'author': "",
+    'website': "",
+    'category': 'Accounting',
+    'version': '14.0.1.0.0',
+    'depends': ['account_asset', 'lease_management'],
+
+    'data':
+        [
+            'views/asset_sell_inherit_view.xml',
+        ]
+}
---
+++ assets_partial_sale/models/__init__.py
@@ -0,0 +1,3 @@
+from . import asset_sell_partial
+from . import account_asset_inherit
+from . import analytic_account_types_asset_inherit
