ae85cb102471a683f826310c40ce9dbc264b477e
cybrosys <cybrosys@cybrosy>
2022-12-06 20:11:22 +0530

    [UPDATE] asset and lease disposal based on date select

--- assets_partial_sale/models/asset_sell_partial.py
+++ assets_partial_sale/models/asset_sell_partial.py
@@ -12,7 +12,14 @@ class AssetSellPartial(models.TransientModel):
         self.ensure_one()
         invoice_line = self.env[
             'account.move.line'] if self.action == 'dispose' else self.invoice_line_id or self.invoice_id.invoice_line_ids
+        if self._context['active_model'] == 'leasee.contract':
+            leasee_contract = self.env['leasee.contract'].browse(
+                int(self._context['active_id']))
+            leasee_contract.termination_date = self.contract_end_date
+        date = invoice_line.move_id.invoice_date
+        if self.action == 'dispose':
+            date = self.contract_end_date
         return self.asset_id.set_to_close(invoice_line_id=invoice_line,
                                           partial=self.partial_bool,
                                           partial_amount=self.partial_amount,
-                                          date=invoice_line.move_id.invoice_date)
+                                          date=date)
