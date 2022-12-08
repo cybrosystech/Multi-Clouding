ee41900fc98eadd35fcd17899f4c1c44b5bf88c0
cybrosys <cybrosys@cybrosy>
2022-12-08 17:40:15 +0530

    [ADD] Account_move_indexing

---
+++ account_move_indexing/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Journal Reversal Indexing',
+    'summary': """""",
+
+    'description': """""",
+
+    'author': "",
+    'website': "",
+    'category': 'Accounting',
+    'version': '14.0.1.0.0',
+    'depends': ['account'],
+
+    'data':
+        [
+
+        ]
+}
