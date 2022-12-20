091ac6a92285478369f1900709c2a0750ba01b41
cybrosys <cybrosys@cybrosy>
2022-12-20 16:18:11 +0530

    [UPDATE] Approve_status account_journal_check

--- approve_status/model/account_move_inherit.py
+++ approve_status/model/account_move_inherit.py
@@ -108,7 +108,7 @@ class AccountMoveInherit(models.Model):
                  ('company_id', '=', self.env.company.id)], limit=1)
             if self.move_type == 'entry':
                 max_value = max(self.line_ids.mapped(
-                    'local_subtotal'))  # Old Field is debit
+                    'debit'))  # Old Field is debit
             else:
                 max_value = sum(self.invoice_line_ids.mapped('local_subtotal'))
             for rec in in_budget.budget_line_ids:
