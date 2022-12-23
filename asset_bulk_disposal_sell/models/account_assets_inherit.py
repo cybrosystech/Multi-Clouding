78c353c6486899d7326dc991911497331da3ef2b
cybrosys <cybrosys@cybrosy>
2022-12-23 14:11:45 +0530

    [UPDATE] asset_bulk_disposal_sell wizard and validation

--- asset_bulk_disposal_sell/models/account_assets_inherit.py
+++ asset_bulk_disposal_sell/models/account_assets_inherit.py
@@ -2,7 +2,6 @@ from odoo import models, fields, api, _
 from math import copysign
 from odoo.tools import float_compare
 from odoo.exceptions import UserError
-from odoo.addons.lease_management.models.account_asset import AccountAsset
 
 
 class AccountAssetBulkSaleDisposal(models.Model):
@@ -19,7 +18,8 @@ class AccountAssetBulkSaleDisposal(models.Model):
                 })
             else:
                 asset_bulk = self.env['asset.sell.disposal.lines'].create({
-                    'asset_id': rec.id
+                    'asset_id': rec.id,
+                    'action': 'dispose',
                 })
             abc.append(asset_bulk.id)
         dd = self.env['asset.bulk.wizard'].create({
@@ -34,17 +34,6 @@ class AccountAssetBulkSaleDisposal(models.Model):
             'target': 'new',
             'res_id': dd.id,
         }
-        # return {
-        #     'name': 'Asset Bulk sale',
-        #     'res_model': 'asset.bulk.wizard',
-        #     'views': [(view.id, 'form')],
-        #     'view_id': view.id,
-        #     'type': 'ir.actions.act_window',
-        #     'target': 'new'
-        # }
-        # for asset in self:
-        #     print(asset)
-        #     asset.action_set_to_close()
 
     def set_to_close_bulk(self, invoice_line_id, partial, partial_amount, date=None):
         self.ensure_one()
@@ -346,108 +335,3 @@ class AccountAssetBulkSaleDisposal(models.Model):
                          ('state', '=', 'draft')]).ids
             # return move_ids
 
-
-class AssetBulkWizard(models.TransientModel):
-    _name = 'asset.bulk.wizard'
-
-    asset_sell_disposal_ids = fields.One2many('asset.sell.disposal.lines', 'asset_bulk_wizard_id')
-
-    def action_apply(self):
-        print(self.asset_sell_disposal_ids,'.........')
-        for record in self.asset_sell_disposal_ids:
-            print('/////', record)
-            record.ensure_one()
-            invoice_line = self.env[
-                'account.move.line'] if record.action == 'dispose' else record.invoice_line_id or record.invoice_id.invoice_line_ids
-            date = invoice_line.move_id.invoice_date
-            if record.action == 'dispose':
-                date = record.contract_end_date
-            record.asset_id.set_to_close_bulk(
-                invoice_line_id=invoice_line if record.invoice_line_id or record.action == 'dispose' else record.invoice_id,
-                partial=record.partial_bool,
-                partial_amount=record.partial_amount,
-                date=date)
-
-
-class AssetSellDisposalLines(models.TransientModel):
-    _name = 'asset.sell.disposal.lines'
-
-    asset_bulk_wizard_id = fields.Many2one('asset.bulk.wizard')
-
-    asset_id = fields.Many2one('account.asset', required=True)
-    company_id = fields.Many2one('res.company',
-                                 default=lambda self: self.env.company)
-
-    action = fields.Selection([('sell', 'Sell'), ('dispose', 'Dispose')],
-                              required=True, default='sell')
-    partial_bool = fields.Boolean('Partial')
-    partial_amount = fields.Float('amount')
-    invoice_id = fields.Many2one('account.move', string="Customer Invoice",
-                                 help="The disposal invoice is needed in order to generate the closing journal entry.",
-                                 domain="[('move_type', 'in', ['out_invoice','in_refund']), ('state', '=', 'posted')]")
-    invoice_line_id = fields.Many2one('account.move.line',
-                                      help="There are multiple lines that could be the related to this asset",
-                                      domain="[('move_id', '=', invoice_id), ('exclude_from_invoice_tab', '=', False)]")
-    select_invoice_line_id = fields.Boolean(
-        compute="_compute_select_invoice_line_id")
-    gain_account_id = fields.Many2one('account.account',
-                                      domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
-                                      compute="_compute_accounts",
-                                      inverse="_inverse_gain_account",
-                                      compute_sudo=True,
-                                      help="Account used to write the journal item in case of gain",
-                                      readonly=False)
-    loss_account_id = fields.Many2one('account.account',
-                                      domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
-                                      compute="_compute_accounts",
-                                      inverse="_inverse_loss_account",
-                                      compute_sudo=True,
-                                      help="Account used to write the journal item in case of loss",
-                                      readonly=False)
-
-    gain_or_loss = fields.Selection(
-        [('gain', 'Gain'), ('loss', 'Loss'), ('no', 'No')],
-        compute='_compute_gain_or_loss',
-        help="Technical field to know is there was a gain or a loss in the selling of the asset")
-    from_leasee_contract = fields.Boolean(default=False)
-    contract_end_date = fields.Date(required=False, )
-
-    @api.depends('company_id')
-    def _compute_accounts(self):
-        for record in self:
-            record.gain_account_id = record.company_id.gain_account_id
-            record.loss_account_id = record.company_id.loss_account_id
-
-    def _inverse_gain_account(self):
-        for record in self:
-            record.company_id.sudo().gain_account_id = record.gain_account_id
-
-    def _inverse_loss_account(self):
-        for record in self:
-            record.company_id.sudo().loss_account_id = record.loss_account_id
-
-    @api.depends('invoice_id', 'action')
-    def _compute_select_invoice_line_id(self):
-        for record in self:
-            record.select_invoice_line_id = record.action == 'sell' and len(
-                record.invoice_id.invoice_line_ids) > 1
-
-    @api.onchange('action')
-    def _onchange_action(self):
-        if self.action == 'sell' and self.asset_id.children_ids.filtered(
-                lambda a: a.state in ('draft', 'open') or a.value_residual > 0):
-            raise UserError(
-                _("You cannot automate the journal entry for an asset that has a running gross increase. Please use 'Dispose' on the increase(s)."))
-
-    @api.depends('asset_id', 'invoice_id', 'invoice_line_id')
-    def _compute_gain_or_loss(self):
-        for record in self:
-            line = record.invoice_line_id or len(
-                record.invoice_id.invoice_line_ids) == 1 and record.invoice_id.invoice_line_ids or \
-                   self.env['account.move.line']
-            if record.asset_id.value_residual < abs(line.balance):
-                record.gain_or_loss = 'gain'
-            elif record.asset_id.value_residual > abs(line.balance):
-                record.gain_or_loss = 'loss'
-            else:
-                record.gain_or_loss = 'no'
