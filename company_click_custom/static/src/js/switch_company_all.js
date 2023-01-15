6df99abb528933ae9a98b184984c405ecb51bea1
sajna@cybrosys.in <sajna@cybrosys.in>
2023-01-15 21:35:17 +0530

    [FIX] company click custom [add] deselect

--- company_click_custom/static/src/js/switch_company_all.js
+++ company_click_custom/static/src/js/switch_company_all.js
@@ -2,24 +2,36 @@ odoo.define('company_click_custom.switch_company_all', function(require) {
 "use strict";
 var SwitchCompanyMenu = require('web.SwitchCompanyMenu');
 var session = require('web.session');
-console.log('SwitchCompanyMenu', SwitchCompanyMenu)
 
 SwitchCompanyMenu.include ({
     events: _.extend({}, SwitchCompanyMenu.prototype.events, {
         'click .select_companies': 'on_select_all',
+        'click .deselect_companies': 'de_select_all',
     }),
 
     on_select_all: function (ev) {
-        console.log('heee', this.$el.find('.dropdown-item'))
-        var dropdownelement = this.$el.find('.dropdown-item')
-        var allowed_company_ids = this.allowed_company_ids;
-        for (let i = 0; i < this.$el.find('.dropdown-item').length; i++) {
-            var dropdownItem = dropdownelement[i]
-            var companyID = dropdownItem.attributes['data-company-id'].value;
-//            var allowed_company_ids = this.allowed_company_ids;
+        var dropdownelement = $(ev.currentTarget.parentElement).find('.dropdown-item')
+        for (let i = 0; i < dropdownelement.length; i++) {
+            var dropdownItem = $(dropdownelement[i]);
+            var companyID = dropdownItem.data('company-id');
+            var allowed_company_ids = this.allowed_company_ids;
             var current_company_id = allowed_company_ids[0];
             allowed_company_ids.push(companyID);
-            this.$el.find('.fa-square-o').removeClass('fa-square-o').addClass('fa-check-square');
+            dropdownItem.find('.fa-square-o').removeClass('fa-square-o').addClass('fa-check-square');
+            $(ev.currentTarget).attr('aria-checked', 'true');
+        }
+        session.setCompanies(current_company_id, allowed_company_ids);
+    },
+    de_select_all: function (ev) {
+        var dropdownelement = $(ev.currentTarget.parentElement).find('.dropdown-item')
+        for (let i = 0; i < dropdownelement.length; i++) {
+            var dropdownItem = $(dropdownelement[i]);
+            var companyID = dropdownItem.data('company-id');
+            var allowed_company_ids = this.allowed_company_ids;
+            var current_company_id = allowed_company_ids[0];
+            allowed_company_ids.splice(allowed_company_ids.indexOf(companyID), 1);
+            dropdownItem.find('.fa-check-square').addClass('fa-square-o').removeClass('fa-check-square');
+            $(ev.currentTarget).attr('aria-checked', 'false');
 
         }
         session.setCompanies(current_company_id, allowed_company_ids);
