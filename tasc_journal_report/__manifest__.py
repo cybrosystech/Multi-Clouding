2726adfe543ac9816105c1cd3c25ea63608a3779
sajna@cybrosys.in <sajna@cybrosys.in>
2023-01-24 19:10:24 +0530

    [ADD] tasc_journal_report

---
+++ tasc_journal_report/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Tasc Journals Report',
+    'summary': """""",
+
+    'description': """""",
+
+    'author': "",
+    'website': "",
+    'category': 'Accounting',
+    'version': '14.0.1.0.0',
+    'depends': ['account', 'base'],
+
+    'data':
+        [
+            'data/journals_report_action.xml',
+            'views/journal_report.xml',
+        ],
+
+    'qweb': [],
+}
