846524bba6f02ff7615c51abc36f39b371c619c6
sajna@cybrosys.in <sajna@cybrosys.in>
2023-04-06 19:27:14 +0530

    [UPDATE] consolidation_elimination_conf

---
+++ consolidation_elimination_conf/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Consolidation Eliminate Configuration',
+    'summary': """""",
+
+    'description': """""",
+
+    'author': "",
+    'website': "",
+    'category': 'Accounting',
+    'version': '14.0.1.0.0',
+    'depends': ['account', 'account_consolidation'],
+
+    'data':
+        [
+            'security/ir.model.access.csv',
+            'views/consolidation_period_view_inherit.xml',
+            'views/elimination_journal_conf_view.xml',
+        ]
+}
