23ae952996528e14404eb181eebcdd389b2a3f17
sajna@cybrosys.in <sajna@cybrosys.in>
2023-04-12 14:36:03 +0530

    [UPDATE] consolidation_elimination_conf name search of same chart

--- consolidation_elimination_conf/models/consolidation_period_inherit.py
+++ consolidation_elimination_conf/models/consolidation_period_inherit.py
@@ -6,11 +6,11 @@ class ConsolidationPeriodElimination(models.Model):
 
     def action_generate_elimination_journal(self):
         eliminated_journal = self.env['consolidation.journal'].search([(
-            'name', '=', self.name),
+            'name', '=', 'Elimination '+self.name),
             ('chart_id', '=', 'Elimination '+self.chart_id.id)])
         if not eliminated_journal:
             eliminated_journal = self.env['consolidation.journal'].create({
-                'name': 'Elimination '+self.chart_id.id,
+                'name': 'Elimination '+self.name,
                 'period_id': self.id,
                 'chart_id': self.chart_id.id
             })
