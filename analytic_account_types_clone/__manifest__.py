57279c2d35ec8849c775174d8603d301ef6730ea
sajna@cybrosys.in <sajna@cybrosys.in>
2023-06-07 16:20:30 +0530

    [UPDATE] analytic_account_types_clone site address

--- analytic_account_types_clone/__manifest__.py
+++ analytic_account_types_clone/__manifest__.py
@@ -8,10 +8,12 @@
     'website': "",
     'category': 'Accounting',
     'version': '14.0.1.0.0',
-    'depends': ['analytic_account_types'],
+    'depends': ['analytic_account_types', 'analytic',
+                'analytic_account_colocation_type', 'account_asset'],
 
     'data':
         [
+            'views/account_analytic_view_form_inherit.xml'
         ],
 
     'qweb': [
