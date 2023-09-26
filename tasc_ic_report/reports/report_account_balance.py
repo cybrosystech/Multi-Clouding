ad6b34a9b3317c828de566be68ce3a41277ad735
CybroOdooDev <cybroodoo@gmail.com>
2023-09-26 13:05:22 +0530

    [ADD] tasc_ic_report

---
+++ tasc_ic_report/reports/report_account_balance.py
@@ -0,0 +1,32 @@
+from odoo import api, models
+
+
+class ReportAccountBalance(models.AbstractModel):
+    _name = 'report.tasc_ic_report.report_account_balance'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['account.account'].browse(data["account_ids"])
+        wizard = self.env['account.ic.balance.report.wizard'].browse(
+            data['context']['active_id'])
+        self._cr.execute(
+            'select account.name,'
+            'ABS(coalesce((sum(item.debit) -sum(item.credit)), 0)) as balance '
+            'from account_move_line as item inner join account_account as '
+            'account on account.id=item.account_id where '
+            'item.account_id in %(accounts)s  and '
+            'item.date<=%(to_date)s and item.company_id=%(company)s and '
+            'item.parent_state=%(state)s group by account.id',
+            {'accounts': tuple(docs.ids),
+             'to_date': data["ending_balance_date"],
+             'company': wizard.company_id.id,
+             'state': 'posted'})
+        qry = self._cr.dictfetchall()
+        return {
+            'doc_ids': docids,
+            'doc_model': 'account.account',
+            'docs': docs,
+            'data': data,
+            'res': qry,
+            'cmpny': wizard.company_id,
+        }
---
+++ tasc_ic_report/wizards/account_balance_report_wizard.py
@@ -0,0 +1,30 @@
+from odoo import models, fields
+
+
+class AccountBalanceReportWizard(models.TransientModel):
+    _name = 'account.ic.balance.report.wizard'
+    _description = 'Account Ending Balance Report'
+
+    company_id = fields.Many2one('res.company', string="Company", required=True,
+                                 default=lambda self: self.env.company)
+    account_ids = fields.Many2many('account.account', string="Accounts",
+                                   domain="[('company_id', '=', company_id)]")
+    ending_balance_date = fields.Date(string="Ending Balance Date",
+                                      default=lambda
+                                          self: fields.Datetime.now().date(),
+                                      required=True)
+    currency_id = fields.Many2one('res.currency', string="Currency",
+                                  related='company_id.currency_id')
+
+    def generate_pdf_report(self):
+        docids = self.env['account.account'].search(
+            [('id', 'in', self.account_ids.ids)])
+        data = {
+            'company': self.company_id,
+            'account_ids': self.account_ids.ids,
+            'ending_balance_date': self.ending_balance_date,
+            'currency': self.currency_id.name,
+        }
+        return self.env.ref(
+            'tasc_ic_report.action_account_balance_reports_pdf').report_action(
+            docids, data=data)
