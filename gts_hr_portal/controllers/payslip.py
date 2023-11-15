a4e69302aeeaa7b655dc5f306e10d2a697338a6f
chiragparakh <88708443+chiragparakh@users.noreply.github.com>
2023-11-15 09:31:18 +0400

    Add files via upload

---
+++ gts_hr_portal/controllers/payslip.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import binascii
+from datetime import date,datetime
+
+from odoo import fields, http, _
+from odoo.exceptions import AccessError, MissingError
+from odoo.http import request
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.osv import expression
+from odoo.exceptions import UserError, AccessError, ValidationError
+
+from odoo.tools import ustr, consteq
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class CustomerPortal(CustomerPortal):
+
+
+    def _prepare_home_portal_values(self, counters):
+        values = super(CustomerPortal, self)._prepare_home_portal_values(counters)
+        payslip_count = 0
+        Employee = request.env['hr.employee']
+        PayslipObj = request.env['hr.payslip']
+        employee = Employee.search([('user_id', '=', request.env.user.id)], limit=1)
+        if employee:
+            payslip = PayslipObj.search([('employee_id', '=', employee.name)])
+            payslip_count = len(payslip)
+        values.update({
+            'payslip_count': payslip_count,
+        })
+        return values
+
+    @http.route(['/my/payslip', '/my/payslip/page/<int:page>'], type='http', auth="user", website=True)
+    def portal_my_payslip(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        Employee = request.env['hr.employee']
+        PayslipObj = request.env['hr.payslip']
+        employee = Employee.search([('user_id', '=', request.env.user.id)], limit=1)
+        domain = [('employee_id', '=', employee.id),('state', '=', 'done')]
+
+        archive_groups = self._get_archive_groups('hr.payslip', domain) if values.get('my_details') else []
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # count for pager
+        payslip_count = PayslipObj.search_count(domain)
+        # pager
+        pager = portal_pager(
+            url="/my/payslip",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=payslip_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager and archive selected
+        payslip = PayslipObj.search(domain, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_payslip_history'] = payslip.ids[:100]
+        values.update({
+            'date': date_begin,
+            'payslip': payslip.sudo(),
+            'page_name': 'payslip',
+            'pager': pager,
+            'archive_groups': archive_groups,
+            'default_url': '/my/payslip',
+        })
+        return request.render("gts_hr_portal.portal_my_payslip", values)
+
+
+
+    @http.route('/payslip/pdf', methods=['POST', 'GET'], csrf=False, type='http', auth="user", website=True)
+    def print_payslip(self, **kw):
+        pdf = request.env['hr.payslip'].sudo()._get_pdf_reports()
+        pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', len(pdf))]
+        return request.make_response(pdf, headers=pdfhttpheaders)
+
---
+++ gts_hr_portal/models/hr_employee.py
@@ -0,0 +1,65 @@
+
+from odoo import models, api,fields, _
+from odoo.tools import email_split
+from odoo.exceptions import UserError
+
+
+def extract_email(email):
+    """ extract the email address from a user-friendly email address """
+    addresses = email_split(email)
+    return addresses[0] if addresses else ''
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    marital = fields.Selection([
+        ('single', 'Single'),
+        ('married', 'Married'),
+        ('cohabitant', 'Legal Cohabitant'),
+        ('widower', 'Widower'),
+        ('divorced', 'Divorced')
+    ], string='Marital Status', groups="hr.group_hr_user,base.group_portal", default='single', tracking=True)
+    children = fields.Integer(string='Number of Children', groups="base.group_portal,hr.group_hr_user", tracking=True)
+    bank_account_id = fields.Many2one(
+        'res.partner.bank', 'Bank Account Number',
+        domain="[('partner_id', '=', address_home_id), '|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+        groups="base.group_portal,hr.group_hr_user",
+        tracking=True,
+        help='Employee bank salary account')
+    worked_days_line_ids = fields.One2many(
+        'hr.payslip.worked_days', 'payslip_id', string='Payslip Worked Days', copy=True,
+        compute='_compute_worked_days_line_ids', store=True, readonly=False,
+        states={'done': [('readonly', True)], 'cancel': [('readonly', True)], 'paid': [('readonly', True)]})
+    identification_id = fields.Char(string='Identification No', groups="base.group_portal,hr.group_hr_user", tracking=True)
+
+    first_contract_date = fields.Date(compute='_compute_first_contract_date', groups="base.group_portal,hr.group_hr_user", store=True)
+
+    contract_id = fields.Many2one(
+        'hr.contract', string='Current Contract', groups="base.group_portal,hr.group_hr_user",
+        domain="[('company_id', '=', company_id), ('employee_id', '=', id)]", help='Current contract of the employee')
+
+    @api.model
+    def create(self, vals):
+        employee = super(HrEmployee, self).create(vals)
+        if not employee.user_id:
+            if not employee.work_email:
+                raise UserError(_('Work email is not defined for employee %s !') % employee.name)
+            user = employee.sudo()._create_user()
+            group_portal = self.env.ref('base.group_portal')
+            group_public = self.env.ref('base.group_public')
+            user.write({'active': True, 'groups_id': [(4, group_portal.id), (3, group_public.id)]})
+            employee.user_id = user.id
+        return employee
+
+    def _create_user(self):
+        company = self.env.user.company_id
+        company = self.company_id or company
+        return self.env['res.users'].with_context(no_reset_password=True).sudo()._create_user_from_template({
+            'email': extract_email(self.work_email),
+            'login': extract_email(self.work_email),
+            'name': self.name,
+            'partner_id': self.address_home_id and self.address_home_id.id or False,
+            'company_id': company.id,
+            'company_ids': [(6, 0, [company.id])],
+        })
