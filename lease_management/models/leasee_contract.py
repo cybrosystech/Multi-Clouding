237efc3125781c108fbfc4d7b9baed77fe571f5b
CybroOdooDev <cybroodoo@gmail.com>
2023-11-27 17:30:04 +0530

    [UPDATE] lease_management

--- lease_management/models/leasee_contract.py
+++ lease_management/models/leasee_contract.py
@@ -225,30 +225,6 @@ class LeaseeContract(models.Model):
                 rec.estimated_ending_date = rec.commencement_date + relativedelta(
                     months=rec.lease_contract_period, days=-1)
 
-    # @api.onchange('analytic_account_id', 'project_site_id', 'type_id',
-    #               'location_id')
-    # def onchange_analytical_account(self):
-    #     if self.state != 'draft':
-    #         account_move_lines = self.env['account.move'].search(
-    #             [('leasee_contract_id', '=', self._origin.id),
-    #              ('state', '=', 'draft')]).mapped(lambda x: x.line_ids)
-    #         # print(len(account_moves), account_moves)
-    #         if account_move_lines:
-    #             for rec in account_move_lines:
-    #                 rec.update({
-    #                     'analytic_account_id': self.analytic_account_id.id,
-    #                     'project_site_id': self.project_site_id.id,
-    #                     'type_id': self.type_id.id,
-    #                     'location_id': self.location_id.id
-    #                 })
-    #         if self.asset_id:
-    #             self.asset_id.update({
-    #                 'account_analytic_id': self.analytic_account_id.id,
-    #                 'project_site_id': self.project_site_id.id,
-    #                 'type_id': self.type_id.id,
-    #                 'location_id': self.location_id.id
-    #             })
-
     @api.onchange('project_site_id')
     def get_location_and_types(self):
         for rec in self:
@@ -742,11 +718,7 @@ class LeaseeContract(models.Model):
                 'model_id': self.asset_model_id.id,
                 'original_value': self.rou_value,
                 'asset_type': 'purchase',
-                # 'partner_id': self.vendor_id.id,
-                # 'company_id': record.move_id.company_id.id,
-                # 'currency_id': self.env.user.company_id.currency_id.id,
                 'acquisition_date': self.commencement_date,
-                # 'method_number': self.lease_contract_period,
                 'currency_id': self.leasee_currency_id.id,
                 'method_number': method_number,
                 'account_analytic_id': self.analytic_account_id.id,
@@ -756,16 +728,10 @@ class LeaseeContract(models.Model):
                 'prorata': self.prorata,
                 'state': 'draft',
                 'first_depreciation_date': self.commencement_date,
-                # 'method_period': self.lease_contract_period_type,
                 'method_period': '1',
             }
-            # changed_vals = self.env['account.asset'].onchange_category_id_values(self.asset_model_id.category_id.id)
-            # vals.update(changed_vals['value'])
-            # vals.update({
-            #     '':,
-            # })
+
             if self.asset_model_id:
-                # asset = self.asset_model_id.copy(vals)
                 vals.update({
                     'account_asset_id': self.asset_model_id.account_asset_id.id,
                     'account_depreciation_id': self.asset_model_id.account_depreciation_id.id,
@@ -993,7 +959,7 @@ class LeaseeContract(models.Model):
                                                                    self.company_id.currency_id,
                                                                    self.company_id,
                                                                    self.commencement_date),
-                                  2)
+                                  3)
 
             lines.append((0, 0, {
                 'name': 'create contract number %s' % self.name,
@@ -1056,27 +1022,8 @@ class LeaseeContract(models.Model):
                     'location_id': self.location_id.id,
                     'currency_id': self.leasee_currency_id.id
                 }))
-        tot_credit = 0
-        tot_debit = 0
 
-        for line in lines:
-            tot_credit = tot_credit + line[2]['credit']
-            tot_debit = tot_debit + line[2]['debit']
-        balance = abs(tot_debit) - abs(tot_credit)
-        if balance:
-            lines.append((0, 0, {
-                'name': 'create contract number %s' % self.name,
-                'account_id': self.lease_liability_account_id.id,
-                'debit': 0,
-                'credit': balance if balance > 0 else 0,
-                'analytic_account_id': self.analytic_account_id.id,
-                'project_site_id': self.project_site_id.id,
-                'type_id': self.type_id.id,
-                'location_id': self.location_id.id,
-                'currency_id': self.leasee_currency_id.id
-            }))
-
-        move = self.env['account.move'].create({
+        self.env['account.move'].create({
             'partner_id': self.vendor_id.id,
             'move_type': 'entry',
             'currency_id': self.leasee_currency_id.id,
@@ -1401,16 +1348,6 @@ class LeaseeContract(models.Model):
 
         return self.asset_id.action_set_to_close()
 
-        # self.process_termination()
-        # view_form = {
-        #     'name': _('Dispose Asset and terminate contract'),
-        #     'view_type': 'form',
-        #     'view_mode': 'form',
-        #     'res_model': 'account.payment',
-        #     'type': 'ir.actions.act_window',
-        #     'target': 'new',
-        # }
-        # return view_form
 
     def get_interest_amount_termination_amount(self, termination_date):
         if not self.installment_ids.filtered(
@@ -1887,11 +1824,6 @@ class LeaseeContract(models.Model):
         install_period = installment.get_period_order()
         installments_per_year = self.get_installments_per_year()
         if self.payment_method == 'beginning':
-            # if self.is_contract_not_annual():
-            #     next_installment = self.installment_ids.filtered(lambda i: i.get_period_order() == (install_period + installments_per_year) and i.get_period_order() > 1)
-            #     current_installment = self.installment_ids.filtered(lambda i: i.get_period_order() == (install_period + installments_per_year - 1) and i.get_period_order() > 1)
-            #     amount = next_installment.amount - current_installment.subsequent_amount if install_period >= 1 else 0
-            # else:
             if install_period == 1:
                 installments = self.installment_ids.filtered(
                     lambda i: i.amount and i.get_period_order() <= (
