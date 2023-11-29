9801288193ad1d8b9175fa6b9cd095924d8e14db
CybroOdooDev <cybroodoo@gmail.com>
2023-11-29 12:30:16 +0530

    [UPDATE] tasc_hr

--- tasc_hr/models/hr_employee.py
+++ tasc_hr/models/hr_employee.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import models, fields
 
 
 class HrEmployee(models.Model):
@@ -6,7 +6,18 @@ class HrEmployee(models.Model):
 
     child_below_18 = fields.Integer(string="Children below 18")
     child_below_4 = fields.Integer(string="Children below 4")
+    spouse_working = fields.Selection(string="Spouse Working",
+                                      selection=[('working', 'Working'),
+                                                 ('not_working', 'Not Working'),
+                                                 ], default='not_working')
 
-
-
-
+#     date_of_birth_ids = fields.One2many('date.of.birth.line', 'employee_id',
+#                                         copy=False)
+#
+#
+# class DateOfBirthLine(models.Model):
+#     _name = 'date.of.birth.line'
+#     _description = 'Children - Date of Birth'
+#
+#     dob = fields.Date(string="Date of Birth")
+#     employee_id = fields.Many2one('hr.employee')
