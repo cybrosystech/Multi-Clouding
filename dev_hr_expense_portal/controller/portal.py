61e1aa51960126ef4001c602de388dda3d45d85f
CybroOdooDev <cybroodoo@gmail.com>
2023-12-10 16:46:00 +0530

    [UPDATE] dev_hr_expense_portal

--- dev_hr_expense_portal/controller/portal.py
+++ dev_hr_expense_portal/controller/portal.py
@@ -201,11 +201,11 @@ class CustomerPortal(CustomerPortal):
             [])
         cost_centers = request.env['account.analytic.account'].sudo().search(
             [('analytic_account_type', '=', 'cost_center'),
-             '|', ('company_id', '=', request.env.company.id), '|',
+             '|', ('company_id', '=', request.env.company.id),
              ('company_id', '=', False)])
         project_sites = request.env['account.analytic.account'].sudo().search(
             [('analytic_account_type', '=', 'project_site'),
-             '|', ('company_id', '=', request.env.company.id), '|',
+             '|', ('company_id', '=', request.env.company.id),
              ('company_id', '=', False)])
 
         values.update({
