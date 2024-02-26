0ec3880006bb709c748a79176c0c41ea86d890e2
CybroOdooDev <cybroodoo@gmail.com>
2024-02-26 19:05:34 +0530

    [UPDATE] gts_hr_portal

--- gts_hr_portal/models/hr_payslip.py
+++ gts_hr_portal/models/hr_payslip.py
@@ -4,6 +4,114 @@ from odoo import models, _, fields
 from odoo.exceptions import ValidationError
 from odoo.tools.safe_eval import safe_eval
 from odoo.tools import float_compare, float_is_zero
+from odoo.addons.hr_payroll_account.models.hr_payroll_account import HrPayslip
+
+
+class HrPayslipMpatch(HrPayslip):
+    def _prepare_line_values(self, line, account_id, date, debit, credit,amt_currency):
+        print("_prepare_line_values")
+        return {
+            'name': line.name,
+            'partner_id': line.partner_id.id,
+            'account_id': account_id,
+            'journal_id': line.slip_id.struct_id.journal_id.id,
+            'date': date,
+            'debit': debit,
+            'credit': credit,
+            'analytic_account_id': line.salary_rule_id.analytic_account_id.id or line.slip_id.contract_id.analytic_account_id.id,
+            'currency_id': line.slip_id.currency_id.id if line.slip_id.currency_id.id != self.env.company.currency_id.id else self.env.company.currency_id.id ,
+            'amount_currency':amt_currency,
+        }
+
+    HrPayslip._prepare_line_values = _prepare_line_values
+
+    def _prepare_slip_lines(self, date, line_ids):
+        print("_prepare_slip_lines")
+        self.ensure_one()
+        precision = self.env['decimal.precision'].precision_get('Payroll')
+        new_lines = []
+        for line in self.line_ids.filtered(lambda line: line.category_id):
+            print("ccccccccc",line.slip_id.company_id.id)
+            if line.currency_id.id != self.env.company.currency_id.id:
+                print("ffffffffffff")
+                amt = line.currency_id._convert(line.total,
+                                                self.env.company.currency_id,
+                                                line.slip_id.company_id,
+                                                self.compute_date)
+                amount = -amt if self.credit_note else amt
+                amt_cur = line.total
+            else:
+                amount = -line.total if self.credit_note else line.total
+                amt_cur = -line.total
+            # amount = -line.total if self.credit_note else line.total
+            amt_currency = amt_cur
+            if line.code == 'NET':  # Check if the line is the 'Net Salary'.
+
+                for tmp_line in self.line_ids.filtered(
+                        lambda line: line.category_id):
+                    if tmp_line.salary_rule_id.not_computed_in_net:  # Check if the rule must be computed in the 'Net Salary' or not.
+                        if line.currency_id.id != self.env.company.currency_id.id:
+                            print("ffffffffffff")
+                            tmp_amt = tmp_line.currency_id._convert(tmp_line.total,
+                                                            self.env.company.currency_id,
+                                                            tmp_line.slip_id.company_id,
+                                                            self.compute_date)
+                        else:
+                            tmp_amt = tmp_line.total
+
+                        if amount > 0:
+                            amount -= abs(tmp_amt)
+                            amt_currency -=abs(tmp_line.total)
+
+                        elif amount < 0:
+                            amount += abs(tmp_amt)
+                            amt_currency+= abs(tmp_line.total)
+            if float_is_zero(amount, precision_digits=precision):
+                continue
+            debit_account_id = line.salary_rule_id.account_debit.id
+            credit_account_id = line.salary_rule_id.account_credit.id
+
+            if debit_account_id:  # If the rule has a debit account.
+                debit = amount if amount > 0.0 else 0.0
+                credit = -amount if amount < 0.0 else 0.0
+
+                debit_line = self._get_existing_lines(
+                    line_ids + new_lines, line, debit_account_id, debit, credit)
+
+                if not debit_line:
+                    debit_line = self._prepare_line_values(line,
+                                                           debit_account_id,
+                                                           date, debit, credit,amt_currency)
+                    debit_line['tax_ids'] = [(4, tax_id) for tax_id in
+                                             line.salary_rule_id.account_debit.tax_ids.ids]
+                    new_lines.append(debit_line)
+                else:
+                    debit_line['debit'] += debit
+                    debit_line['credit'] += credit
+                    debit_line['amount_currency'] += amt_currency
+
+            if credit_account_id:  # If the rule has a credit account.
+                debit = -amount if amount < 0.0 else 0.0
+                credit = amount if amount > 0.0 else 0.0
+                credit_line = self._get_existing_lines(
+                    line_ids + new_lines, line, credit_account_id, debit,
+                    credit)
+
+                if not credit_line:
+                    credit_line = self._prepare_line_values(line,
+                                                            credit_account_id,
+                                                            date, debit, credit,amt_currency)
+                    credit_line['tax_ids'] = [(4, tax_id) for tax_id in
+                                              line.salary_rule_id.account_credit.tax_ids.ids]
+                    new_lines.append(credit_line)
+                else:
+                    credit_line['debit'] += debit
+                    credit_line['credit'] += credit
+                    credit_line['amount_currency'] += amt_currency
+
+        return new_lines
+
+    HrPayslip._prepare_slip_lines = _prepare_slip_lines
 
 
 class HrPayslip(models.Model):
