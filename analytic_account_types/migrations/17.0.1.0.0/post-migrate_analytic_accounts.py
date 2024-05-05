6a270dcf5cf8d624e4f69a21038e8df40f04323f
CybroOdooDev <cybroodoo@gmail.com>
2024-05-05 10:12:14 +0530

    [UPDATE] analytic_account_types

--- analytic_account_types/migrations/17.0.1.0.0/post-migrate_analytic_accounts.py
+++ analytic_account_types/migrations/17.0.1.0.0/post-migrate_analytic_accounts.py
@@ -23,12 +23,6 @@ def migrate(cr, version):
             'UPDATE account_analytic_account SET plan_id=%s where analytic_account_type=%s',
             [new_record.id, type[0]])
 
-    # cr.execute('UPDATE account_move_line AS am SET analytic_account_id = ('
-    #            'SELECT jsonb_object_keys(analytic_distribution)::integer AS '
-    #            'jsonb_keys FROM account_move_line ab WHERE ab.id = am.id)'
-    #            ' WHERE EXISTS (SELECT 1 FROM account_move_line ab WHERE '
-    #            'ab.id = am.id)')
-
     cr.execute('UPDATE account_move_line AS am '
                'SET analytic_account_id = ('
                'SELECT jsonb_object_keys(analytic_distribution)::integer AS jsonb_keys '
@@ -45,4 +39,20 @@ def migrate(cr, version):
                ')'
                'AND analytic_account_id IS NULL')
     print("gggggggggg")
+    cr.execute('''
+        UPDATE account_move_line
+        SET analytic_distribution = 
+            CASE 
+                WHEN project_site_id IS NOT NULL THEN 
+                    CASE 
+                        WHEN analytic_distribution IS NULL THEN jsonb_build_object(
+                            COALESCE(project_site_id::text, 'default'), '100'
+                        )
+                        ELSE analytic_distribution || jsonb_build_object(
+                            COALESCE(project_site_id::text, 'default'), '100'
+                        )
+                    END
+                ELSE analytic_distribution
+            END
+    ''')
     _logger.info("Completed the post migration process")
