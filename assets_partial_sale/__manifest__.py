9a4eb536458ac735fac30b20a1babb69b10e5936
CybroOdooDev <cybroodoo@gmail.com>
2024-12-26 12:37:55 +0530

    [UPDATE] assets_partial_sale - Fix error in lease termination

--- assets_partial_sale/__manifest__.py
+++ assets_partial_sale/__manifest__.py
@@ -1,21 +1,17 @@
 {
     'name': 'Partial Assets Sale',
-    'summary': """""",
-
-    'description': """""",
-
+    'summary': """Module for asset partial sale.""",
+    'description': """Module for asset partial sale.""",
     'author': "",
     'website': "",
     'category': 'Accounting',
     'version': '17.0.1.0.0',
     'depends': ['account_asset', 'lease_management', 'assets_sub_models_config'],
-
     'data':
         [
             'views/asset_sell_inherit_view.xml',
             'views/account_aaset_form_capex_inherit.xml',
         ],
-    "license": 'AGPL-3',
     'installable': True,
     'auto_install': False,
     'application': False,
