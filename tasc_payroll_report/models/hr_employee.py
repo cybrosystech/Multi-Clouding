ad15bbef42581ea4ff83ba4c07dbaa0b929a87e0
CybroOdooDev <cybroodoo@gmail.com>
2025-01-13 10:34:07 +0530

    [ADD] tasc_dashboard - Fix access issue of users for dashboard modules,Adding purchase record rules, tasc_payroll_report - Added date_of_joining,employment_type to hr public employee, gts_hr_portal -Fix access issue for users for employees,timeoff,expense,etc

--- tasc_payroll_report/models/hr_employee.py
+++ tasc_payroll_report/models/hr_employee.py
@@ -18,3 +18,17 @@ class HrEmployee(models.Model):
     bank_bic = fields.Char(related='bank_id.bic', string="SWIFT/BIC")
     branch = fields.Char(string="Branch", related='bank_account_id.branch')
     bank_iban = fields.Char(string="IBAN", related='bank_account_id.bank_iban')
+
+
+class HrEmployeePublic(models.Model):
+    _inherit = 'hr.employee.public'
+
+    date_of_joining = fields.Date(string="Date of Joining")
+    employment_type = fields.Selection(string="Employment Type",
+                                       selection=[('FTE', 'FTE'),
+                                                  ('Contract', 'Contract'), (
+                                                      'consulatncy_agreement',
+                                                      'Consultancy Agreement'),
+                                                  ('os_ipt', 'OS-IPT'),
+                                                  ('os_sim', 'OS-SIM'),
+                                                  ('fte_capped', 'FTE-Capped')])
